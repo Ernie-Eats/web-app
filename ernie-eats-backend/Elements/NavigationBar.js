8c79223653d056c739d704f659e1f2c522cc17a4
0wenWood <owenwood3435@outlook.com>
2023-11-13 09:59:52 -0500

    Standardized Custom Elements

--- ernie-eats-backend/Elements/NavigationBar.js
+++ ernie-eats-backend/Elements/NavigationBar.js
@@ -1,88 +1,67 @@
 import * as Userdatabase from '../Database/UserDatabase.js';
 
 class NavigationBar extends HTMLElement {
+
+    isContentDisplayed = true;
     constructor() {
         super();
 
-        const shadow = this.attachShadow({ mode: "open" });
+        this.attachShadow({ mode: "open" });
+    }
 
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
         const css = document.createElement("link");
         css.rel = "stylesheet";
         css.href = "./ernie-eats-frontend/CSS/navbar.css";
 
-        const wrapper = document.createElement("nav");
-        wrapper.setAttribute("class", "navigation-wrapper");
+        let account = "./ernie-eats-frontend/Images/defaultLogin.png";
 
-        const homePage = wrapper.appendChild(document.createElement("a"));
-        homePage.href = "index.html";
-        
-        const homeLogo = homePage.appendChild(document.createElement("img"));
-        homeLogo.src = "./ernie-eats-frontend/Images/CoverLogo.jpg";
-        homeLogo.id = "homeLogo-img";
+        this.shadowRoot.innerHTML = `
+            <div class="navigation-wrapper">
+                <a href="index.html"><img src="./ernie-eats-frontend/Images/CoverLogo.jpg" id="homeLogoImg"></a>
+                <input type="text" placeholder="Search..." id="searchBar"> </input>
+                <button class="button"><img src=${account}></button>
+                <div id="hamburger-wrapper">
+                    <button class="hamburger-button"><img src="./ernie-eats-frontend/Images/cake.jpg" id="hamburgerImg"></button>
+                </div>
+            </div>
+        `;
 
-        const searchBar = wrapper.appendChild(document.createElement("input"));
-        searchBar.setAttribute("type", "text");
-        searchBar.setAttribute("placeholder", "Search...");
+        this.shadowRoot.querySelector("button").addEventListener("click", async () => await this.login()); 
 
-        const accountLogin = wrapper.appendChild(document.createElement("button"));
-        accountLogin.setAttribute("class", "button");
+        const hamburgerWrapper = this.shadowRoot.querySelector("#hamburger-wrapper");
+        hamburgerWrapper.querySelector("button").addEventListener("click", async () => await this.hamburgerMenu(hamburgerWrapper));
 
-        const accountPhoto = accountLogin.appendChild(document.createElement("img"));
-        accountPhoto.src = "./ernie-eats-frontend/Images/defaultLogin.png";
+        this.shadowRoot.appendChild(css);
+    }
 
-        const hamburgerWrapper = wrapper.appendChild(document.createElement("div"));
-        hamburgerWrapper.setAttribute("id", "hamburger-wrapper");
-
-        const hamburger = hamburgerWrapper.appendChild(document.createElement("button"));
-        hamburger.setAttribute("class", "hamburger-button");
-
-        const hamburgerImg = hamburger.appendChild(document.createElement("img")); 
-        hamburgerImg.src = "./ernie-eats-frontend/Images/cake.jpg";
-        hamburgerImg.id = "hamburger-img";
-
-        let searchResult = ""; 
-
-        searchBar.oninput = (e) => searchResult = e.target.value;
-
-        document.addEventListener("keypress", (e) => {
-            if (searchResult.length !== 0 && e.key === "Enter") {
-                window.close();
-                window.open('business-page.html');
+    async login() {
+        console.log("Pressed");
+        await Userdatabase.findAllUsers().then(result => {
+            if (result.success) {
+                this.getAddress().then(address => {
+                    let found = result.model.find((value) => value.address === address) !== undefined;
+                    found ? window.open('user-page.html') : window.open('login-Signup.html');
+                    window.close();
+                })
             }
         });
-
-        hamburger.addEventListener("click", () => this.hamburgerMenu(hamburgerWrapper, hamburgerImg));
-
-        accountLogin.addEventListener("click", async () => {
-            await Userdatabase.findAllUsers().then(result => {
-                if (result.success) {
-                    this.getAddress().then(address => {
-                        let found = result.model.find((value) => value.address == address) !== undefined;
-                        found ? window.open('user-page.html') : window.open('login-Signup.html');
-                        window.close();
-                    })
-                }
-            });
-        });
-
-        shadow.appendChild(css);
-        shadow.appendChild(wrapper);
     }
 
     async getAddress() {
         return await fetch('https://api.ipify.org?format=json')
-                        .then(async response => await response.json())
-                        .then(data => { return data.ip });
+            .then(async response => await response.json())
+            .then(data => { return data.ip });
     }
 
-    hamburgerMenu(wrapper, img) {
-        if (!wrapper.getAttribute("data-isContentDisplayed")) {
-            wrapper.setAttribute("data-isContentDisplayed", "true");
-        }
-      
-        const isContentDisplayed = wrapper.getAttribute("data-isContentDisplayed") === "true";
+    async hamburgerMenu(wrapper) {
+        const img = wrapper.querySelector("#hamburgerImg");
         let content;
-        if (isContentDisplayed) {
+        if (this.isContentDisplayed) {
             content = wrapper.appendChild(document.createElement("div"));
             content.setAttribute("id", "content-wrapper");
 
@@ -94,30 +73,10 @@ class NavigationBar extends HTMLElement {
             faqPage.href = "FAQ.html";
             faqPage.innerHTML = "FAQ Page";
 
-            const logoutButton = content.appendChild(document.createElement("button"));
-            logoutButton.innerHTML = "Logout";
-
-            logoutButton.addEventListener("click", async () => {
-                await Userdatabase.findAllUsers().then(result => {
-                    if (result.success) {
-                        this.getAddress().then(address => {
-                            let found = result.model.find((value) => value.address === address);
-                            console.log(found);
-                            if (found !== undefined) {
-                                found.address = "unknown";
-                                console.log(found);
-                                Userdatabase.updateUser(found).then(r => console.log(r));
-                            }
-                        })
-                    }
-                });
-            });
-          
             const settingsPage = content.appendChild(document.createElement("button"));
             settingsPage.innerHTML = "Settings Page";
             settingsPage.addEventListener("click", async () => {
-                await Userdatabase.findAllUsers().then(result => {
-                    console.log(result);
+                await Userdatabase.findAllUsers().then(result => { 
                     if (result.success) {
                         this.getAddress().then(address => {
                             console.log(address);
@@ -126,9 +85,26 @@ class NavigationBar extends HTMLElement {
                                 window.open("generalsettings.html");
                                 window.close();
                             } else {
-                                console.log("You are not logged in!");
+                                alert("Please login to look at your settings page");
                             }
                         });
+                    }
+                });
+            });
+
+            const logoutButton = content.appendChild(document.createElement("button"));
+            logoutButton.innerHTML = "Logout";
+
+            logoutButton.addEventListener("click", async () => {
+                await Userdatabase.findAllUsers().then(result => {
+                    if (result.success) {
+                        this.getAddress().then(address => {
+                            let found = result.model.find((value) => value.address === address);
+                            if (found !== undefined) {
+                                found.address = "";
+                                Userdatabase.updateUser(found).then(r => console.log(r));
+                            }
+                        })
                     }
                 });
             });
@@ -140,7 +116,7 @@ class NavigationBar extends HTMLElement {
             img.src = "./ernie-eats-frontend/Images/cake.jpg";
         }
 
-        wrapper.setAttribute("data-isContentDisplayed", !isContentDisplayed);
+        this.isContentDisplayed = !this.isContentDisplayed;
     }
 } 
 
