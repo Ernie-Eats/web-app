b1753353781b3c1cdb443b6b5c1da3c34ab5755f
0wenWood <owenwood3435@outlook.com>
2023-11-29 10:00:32 -0500

    Added functionality to the create review button

--- ernie-eats-backend/Pages/BusinessPage.js
+++ ernie-eats-backend/Pages/BusinessPage.js
@@ -1,5 +1,7 @@
 import * as ReviewDatabase from '../Database/ReviewsDatabase.js';
 import * as UserDatabase from '../Database/UserDatabase.js';
+import * as Function from '../Database/functions.js';
+import { Review } from '../Database/models.js';
  
 const urlParams = new URLSearchParams(window.location.search);
 const page = urlParams.get("page").replace("%20", " ");
@@ -8,7 +10,65 @@ document.getElementById("name").innerText = page;
 const reviewsDiv = document.getElementById("reviews");
 const rating = document.getElementById("rating");
 const stars = document.getElementById("stars");
+const addReview = document.getElementById("add-review");
+let userId = "";
 let starAverage = 0;
+
+addReview.addEventListener("click", async () => {
+    if (!document.querySelector(".container").classList.contains("blur")) {
+        document.querySelector(".container").classList.add("blur");
+
+        const review = document.createElement("div");
+        review.classList.add("review");
+
+        const title = review.appendChild(document.createElement("input"));
+
+        const stars = review.appendChild(document.createElement("div"));
+
+        for (let i = 0; i < 5; i++) {
+            let star = stars.appendChild(document.createElement("img"));
+            star.src = "./ernie-eats-frontend/Images/unfilledStar.jpg";
+            star.classList.add("unfilled-star");
+            star.onclick = (ev) => clickStar(ev, stars);
+        }
+        review.appendChild(document.createElement("hr"));
+
+        const text = review.appendChild(document.createElement("input"));
+
+        const saveButton = review.appendChild(document.createElement("button"));
+        saveButton.innerText = "Save";
+        saveButton.addEventListener('click', async () => {
+            if (title.value.length === 0 && text.value.length === 0) {
+                alert("Unknown Title and Review");
+            } else {
+                let rating = 0;
+                [...stars.children].forEach(s => {
+                    if (s.classList.contains("filled-star")) {
+                        rating++;
+                    }
+                });
+                await ReviewDatabase.insertReview(new Review(title.value, text.value, rating, id, userId)).then(r => {
+                    if (r.success) {
+                        document.querySelector(".container").classList.toggle("blur");
+                        document.querySelector(".review").remove();
+                    }
+                });
+            }
+        });
+
+        const cancelButton = review.appendChild(document.createElement("button"));
+        cancelButton.innerText = "Cancel";
+        cancelButton.addEventListener("click", () => {
+            document.querySelector(".container").classList.toggle("blur");
+            document.querySelector(".review").remove();
+        })
+        document.body.appendChild(review);
+
+    } else {
+        document.querySelector(".container").classList.toggle("blur");
+        document.querySelector(".review").remove();
+    }
+});
 
 await ReviewDatabase.findReviewsByResturantId(id).then(reviews => {
     if (reviews.success) {
@@ -45,8 +105,51 @@ await ReviewDatabase.findReviewsByResturantId(id).then(reviews => {
     }
 });
 
-console.log(starAverage);
+await Function.getAddress().then(address => {
+    UserDatabase.findUserByAddress(address).then(user => {
+        if (user.success) {
+            userId = user.model.id;
+        }
+    });
+});
 
+function clickStar(ev, stars) {
+    const starList = [...stars.children];
+    starList.forEach(s => {
+        if (s.classList.contains("filled-star")) {
+            s.classList.add("unfilled-star");
+            s.classList.remove("filled-star");
+            s.src = "./ernie-eats-frontend/Images/unfilledStar.jpg";
+        }
+    })
+    starList.forEach((s, index) => {
+        if (s === ev.target) {
+            if (index !== 0) {
+                for (let i = 0; i < index + 1; i++) {
+                    if (starList[i].classList.contains("unfilled-star")) {
+                        starList[i].classList.add("filled-star");
+                        starList[i].classList.remove("unfilled-star");
+                        starList[i].src = "./ernie-eats-frontend/Images/filledStar.jpg";
+                    } else {
+                        starList[i].classList.add("unfilled-star");
+                        starList[i].classList.remove("filled-star");
+                        starList[i].src = "./ernie-eats-frontend/Images/unfilledStar.jpg";
+                    }
+                }
+            } else {
+                if (ev.target.classList.contains("unfilled-star")) {
+                    ev.target.classList.add("filled-star");
+                    ev.target.classList.remove("unfilled-star");
+                    ev.target.src = "./ernie-eats-frontend/Images/filledStar.jpg";
+                } else {
+                    ev.target.classList.add("unfilled-star");
+                    ev.target.classList.remove("filled-star");
+                    ev.target.src = "./ernie-eats-frontend/Images/unfilledStar.jpg";
+                }
+            }
+        }
+    });
+}
 
 function round(num) {
     let rounded = (Number)(num.toFixed(1));
